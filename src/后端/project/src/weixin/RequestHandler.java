5ddde1d71b53ba84333b0febf7ae97981a9afc8a
BSTLEEFT <117899866+BSTLEEFT@users.noreply.github.com>
2022-11-21 15:37:37 +0800

    Add files via upload

---
+++ src/后端/project/src/weixin/RequestHandler.java
@@ -0,0 +1,60 @@
+package com.project.weixin.pay;
+
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import java.util.SortedMap;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 
+ * 
+ * 青岛蓝图科技网络有限公司
+ * http://www.xiaodaofuli.com
+ * 联系方式：137-9192-7167
+ * 技术QQ：2511251392
+ */
+public class RequestHandler {
+	
+	private String charset;
+	/**
+	 * 
+	 * 
+	 * 青岛蓝图科技网络有限公司
+	 * http://www.xiaodaofuli.com
+	 * 联系方式：137-9192-7167
+	 * 技术QQ：2511251392
+	 */
+	public RequestHandler(HttpServletRequest request, HttpServletResponse response) {
+		
+		this.charset = "UTF-8";
+	}
+	/**
+	 * 
+	 * 
+	 * 青岛蓝图科技网络有限公司
+	 * http://www.xiaodaofuli.com
+	 * 联系方式：137-9192-7167
+	 * 技术QQ：2511251392
+	 */
+	@SuppressWarnings("rawtypes")
+	public String createSign(SortedMap<String, String> packageParams, String partnerkey) {
+		
+		StringBuffer sb = new StringBuffer();
+		Set es = packageParams.entrySet();
+		Iterator it = es.iterator();
+		while (it.hasNext()) {
+			Map.Entry entry = (Map.Entry) it.next();
+			String k = (String) entry.getKey();
+			String v = (String) entry.getValue();
+			if (null != v && !"".equals(v) && !"sign".equals(k) && !"key".equals(k)) {
+				sb.append(k + "=" + v + "&");
+			}
+		}
+		sb.append("key=" + partnerkey);
+		String sign = MD5Util.MD5Encode(sb.toString(), this.charset).toUpperCase();
+		return sign;
+	}
+}
