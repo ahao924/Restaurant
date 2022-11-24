881921c05cfb2171635f90c29e09fe087dd93699
BSTLEEFT <117899866+BSTLEEFT@users.noreply.github.com>
2022-11-24 12:25:40 +0800

    Add files via upload

---
+++ src/后端/project/src/com/project/model/AdminGoods.java
@@ -0,0 +1,34 @@
+package com.project.model;
+
+import java.util.List;
+
+import com.jfinal.plugin.activerecord.Db;
+import com.jfinal.plugin.activerecord.Model;
+import com.jfinal.plugin.activerecord.Record;
+
+/**
+ * 
+ * 
+ * 青岛蓝图科技网络有限公司
+ * http://www.xiaodaofuli.com
+ * 联系方式：137-9192-7167
+ * 技术QQ：2511251392
+ */
+public class AdminGoods extends Model<AdminGoods> {
+
+	private static final long serialVersionUID = 1L;
+	public static final AdminGoods dao = new AdminGoods();
+	
+	/**
+	 * 
+	 * 
+	 * 青岛蓝图科技网络有限公司
+	 * http://www.xiaodaofuli.com
+	 * 联系方式：137-9192-7167
+	 * 技术QQ：2511251392
+	 */
+	public static List<Record> getList() {
+
+		return Db.find("select * from db_admin_goods order by month asc");
+	}
+}
